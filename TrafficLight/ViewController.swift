962b4d1d73842c70931fd82460377c967e8689dd
Andrey Klimenko <45719199+KlimEnN@users.noreply.github.com>
2021-08-18 19:50:43 +0300

    new commit

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLamp: UIView!
+    @IBOutlet weak var yellowLamp: UIView!
+    @IBOutlet weak var greenLamp: UIView!
+    @IBOutlet var lamps: [UIView]!
+    
+    @IBOutlet weak var button: UIButton!
+    
+    var currentID = 2
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLamp.backgroundColor = .red
+        yellowLamp.backgroundColor = .yellow
+        greenLamp.backgroundColor = .green
+        
+        button.setTitle("START", for: .normal)
+        
+        shotDownLamp()
+    }
+    
+    @IBAction func switchButton(_ sender: UIButton) {
+        shotDownLamp()
+        
+        lamps[currentID].alpha = 1
+        button.setTitle("NEXT", for: .normal)
+        
+        
+        if currentID > 0 {
+            currentID -= 1
+        } else {
+            currentID = 2
+        }
+    }
+    
+    
+    func shotDownLamp() {
+        for lamp in lamps {
+            lamp.layer.cornerRadius = 75
+            lamp.alpha = 0.1
+        }
     }
 
-
+    
 }
 
